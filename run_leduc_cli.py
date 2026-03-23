df7d75195b18f6866d6e89425d2cba35ff7f23fa
Juan Hernandez <hernandezj27@udayton.edu>
2026-03-23 10:23:39 -0400

    Task 1 complete

--- run_leduc_cli.py
+++ run_leduc_cli.py
@@ -1,15 +1,18 @@
 import rlcard
-from rlcard.agents import RandomAgent
+from rlcard.agents import CFRAgent
 from rlcard.agents.human_agents.leduc_holdem_human_agent import HumanAgent
 
 env = rlcard.make('leduc-holdem')
 
+print("Number of actions:", env.num_actions)
+
 human_agent = HumanAgent(num_actions=env.num_actions)
-ai_agent = RandomAgent(num_actions=env.num_actions)
+ai_agent = CFRAgent(env)
+ai_agent.load()
 
 env.set_agents([human_agent, ai_agent])
 
-trajectories, payoffs = env.run(is_training=False)
-
-print("Game finished.")
-print("Payoffs:", payoffs)
+while True:
+    trajectories, payoffs = env.run(is_training=False)
+    print("Game finished.")
+    print("Payoffs:", payoffs)
